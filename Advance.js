a423c8b43a78f2da49cefda1ab32151fa7cbb41f
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-07 07:04:51 +0500

    Scripting in code

---
+++ Advance.js
@@ -0,0 +1,16 @@
+class Bio {
+  first(name) {
+    alert(`hellol ${name} how are you?? `);
+    document.write('Congratulation' + ' ' +  name + ' ' + "You're access this object oriented programming")
+  }
+  second(name) {
+    alert(`can i give you ${name} data`);
+  }
+}
+
+let Jibran = new Bio();
+let Munaaf = new Bio();
+
+let name = "Jibran";
+Jibran.first(name);
+
